a687f4130b3fb5507cd4f95f6cedf4a5d4101006
Javier <jmanteca@jmanvel.com>
2021-10-06 22:21:48 +0000

    FinisedGameMaster

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
-// we declare a new global variable containing an array that represents the ballons map
-// you have to add more colors into the ballonsMap array
-let ballonsMap = ['green','red','orange','blue','purple','yellow','black','pink'];
 
 
-const popBalloon = (position) => {
-    // delete balloons
-    
-   document.querySelector(`#balloon${position}`).classList.remove('active');
-   document.querySelector(`#balloon${position}`).classList.add('popped');
-   ballonsMap.splice(position,1,null);
-   
-   document.querySelector("#balloon-count").innerHTML = ballonsMap.filter(b => b !== null).length;
-    console.log(ballonsMap)
-   if(ballonsMap.filter(b => b !== null).length == 0) window.location.reload(); // <--- reload website when no more balloons are left
+let count = 0;
+var death;
+const popBalloon = () => {
+   //stop time to dead
+   clearTimeout(death)
+    // delete monster   
+  
+    let end = document.querySelector('.end');
+    let monster = document.querySelector('#monster');
+    end.removeChild(monster)   
+   count = count +1
+   console.log(count)
+  document.querySelector("#balloon-count").innerHTML = count + '€';
+  
+  //put a new rock problem
+  let mountain = document.querySelector('.mountain');
+  let newRock = document.createElement('div');
+  newRock.classList.add('rock');
+  let randomWidth = Math.floor(Math.random() * (90 - 10 + 1) + 10);
+  let randomHeight = Math.floor(Math.random() * (90 - 10 + 1) + 10);
+  newRock.style.left = randomWidth + '%';
+  newRock.style.top = randomHeight + '%';
+  mountain.appendChild(newRock);
+   createMonster() 
+  
 }
 
+const createMonster = () => {
+       //create new monster
 
-
-
-const render = () => {
+       
+       let randomPosition = Math.floor(Math.random() * (90 - 10 + 1) + 10)
     
-    // convert ballons map of colors into real html balloons
-    const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active" id="balloon${position}" onclick="popBalloon(${position})" style="background-color: ${color}"></div>`; // <--- render each balloon
-    });
+       let end = document.querySelector('.end');
+       end.innerHTML = '<span style="left:'+ randomPosition + '%" class="animate__slower animate__animated  animate__infinite animate__backInDown" onclick="popBalloon()" id="monster"><i class="fab fa-optin-monster"></i></span>';
+      
+       //Dead alert with sweetalert
+      death = setTimeout(function(){ 
+      swal('YOU ARE DEAD!','You Win: ' + count + '€','info',{button:"Play Again?"}) .then((value) => {location.reload()})
+      }, 4000);
 
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
-    document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
-
-    
 }
 
-// this makes the "render" function trigger when the website starts existing
-window.onload = render();
+createMonster();
+
